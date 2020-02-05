1d830a21b04c3a07a479e5bb4b782dae0ee3da85
Glitch (ragnorok-zachary) <none>
2020-02-05 13:41:32 +0000

    ⛰🐕 Updated with Glitch

---
+++ src/core/components/my-components.js
@@ -0,0 +1,27 @@
+export default (()=>{
+  
+  document.addEventListener('gameStart',e=>{
+    
+
+  
+  });
+  
+  
+  AFRAME.registerComponent('mycomponent', {
+    
+    
+    schema: {
+      color: {default: 'red'}
+    },
+
+    init: function(){
+
+    }
+    
+    
+  });
+  
+
+
+  
+})()
